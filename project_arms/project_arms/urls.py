13da7c99fe5428dda5ddae4ac3f34247e843ae8b
vamshi <thatipamula2@gmail.com>
2023-12-07 12:21:02 +0530

    updated

--- project_arms/project_arms/urls.py
+++ project_arms/project_arms/urls.py
@@ -16,21 +16,61 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sample import views
 from django.http import HttpResponse
-# def task1(p1):
-#     print("*"*20)
-#     print(p1)
-#     # return "hello world"
-#     # return HttpResponse("Hello world")
-#     resp = """
-#     <html>
-#     <head> <title>SAMPLE</title> </head>
-#     </html>
-#     """
-#     return HttpResponse(resp)
+import psutil
+import multiprocessing
+import platform
+
+
+def ram(request):
+    ram_info = psutil.virtual_memory()
+    result = (
+        f"Total: {ram_info.total / 1024 / 1024 / 1024:.2f} GB<br>"
+        f"Available: {ram_info.available / 1024 / 1024 / 1024:.2f} GB<br>"
+        f"Used: {ram_info.used / 1024 / 1024 / 1024:.2f} GB<br>"
+        f"Percentage usage: {ram_info.percent}%"
+    )
+
+    return HttpResponse(result)
+def os(request):
+    result =(
+        f"The name of os is {platform.system()}<br>"
+        f"The version is {platform.version()}"
+    )
+    return HttpResponse(result)
+
+def ncores(request):
+    result = (
+        f"The number of the cores in this machine are {multiprocessing.cpu_count()}"
+    )
+    return HttpResponse(result)
+
+def users(request):
+    html_code = """
+    <html>
+            <head>
+                <title>
+                    User Page
+                </title>
+            </head>
+            <body>
+                <h1>This is a User page</h1>
+                <pr>Welcome to this page. This is Thatipamula Vamshi</Pr>
+            <body>
+    </html>  
+    """
+    return HttpResponse(html_code)
+
+#print(dir(os.environ))
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    
-    # path("users/", task1),#task1(request_obj)# server parsing that return value. expecting HttpReponse. 
-]
+    path('admin/', admin.site.urls),
+    path('stupid/',views.hello),
+    path('tejus/',views.login, name = 'hello1'),
+    path('ram/',ram),
+    path('os/',os),
+    path('ncores/',ncores),
+    path('users/',users),
+ ]
--- project_arms/sample/views.py
+++ project_arms/sample/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
-# Create your views here.
+def hello(request):
+    value = os.environ.get('crazy')
+    print(dir(os))
+    return HttpResponse(value)
+def login(request):
+    os.environ['tejus'] = 'fighter Jet'
+    value = os.environ.get('tejus')
+    return HttpResponse(value)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
