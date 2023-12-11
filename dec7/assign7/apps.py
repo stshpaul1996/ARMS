7f896c238cfb785fbe3eb999b190976a93652e3a
HarithaAyetha0108 <151164911+HarithaAyetha0108@users.noreply.github.com>
2023-12-11 21:59:12 +0530

    Delete dec7 directory

--- dec7/assign7/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Assign7Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'assign7'
--- dec7/task9app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Task9AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task9app'
