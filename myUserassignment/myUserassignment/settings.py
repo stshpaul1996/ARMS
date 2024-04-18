8746d61477851db88b652726b4c64972375ae681
unknown <gnithisha5k4@gmail.com>
2024-04-18 12:38:54 +0530

    userassignment

--- myUserassignment/myUserassignment/settings.py
+++ myUserassignment/myUserassignment/settings.py
@@ -80,7 +80,15 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    },
+    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'userdatabase',
+    #     'USER': 'nithisha',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'postgres',  # Docker service name
+    #     'PORT': '5432',       # Default PostgreSQL port
+    # }
     # 'read_db': {
     #     'ENGINE': 'djongo',
     #     'NAME': 'read_db_name',
